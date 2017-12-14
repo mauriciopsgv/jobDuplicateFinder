3e7525b7b497753049c4fd7c61c94104e0951ae4
Mauricio Pedro Vieira <mauriciopsgv@gmail.com>
2017-12-14 07:07:37 -0200

    changes to relative path

--- DatasetConvert.py
+++ DatasetConvert.py
@@ -2,18 +2,16 @@ import json
 import csv
 from pprint import pprint
 
-fulldataset = json.load(open("C:/code/jobDuplicateFinder/DatasetTratado.json"))
+fulldataset = json.load(open("DatasetTratado.json"))
 
 # Getting n first entrys
 n = 200
-print(fulldataset[0])
 smallDataset = []
 for a in fulldataset:
     smallDataset.append(a["description"].split() + a["title"].split())
 #    if (fulldataset.index(a) == n - 1):
 #        break
 
-print(smallDataset[0])
 
 filetowrite = open("smallDataset.csv", "w", newline ='')
 writer = csv.writer(filetowrite)
